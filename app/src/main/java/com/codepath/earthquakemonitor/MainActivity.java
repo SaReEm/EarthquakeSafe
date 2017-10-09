f3c207b06f79b318bdbfae9eb64fd36133b5c56d
kri55is <brisseau.emilie@gmail.com>
2017-10-09 15:20:05 -0700

    - build earthquake API client with several calls: getEarthquakeAroundPointWithFilter calls for earthquake around a specific place according to filters buildParamWithFilter is building Parameter to prepare for the request according to the filter - Create a Filters class to have filters on the earthquake queries

--- app/src/main/java/com/codepath/earthquakemonitor/MainActivity.java
+++ app/src/main/java/com/codepath/earthquakemonitor/MainActivity.java
@@ -33,17 +33,6 @@ public class MainActivity extends AppCompatActivity {
                 //PushTest.sendPushTest();
             }
         });
-
-        ////Piece of code to test earthquake API /////////////////////////////////////////
-        EarthquakeClient client = EarthquakeClient.getInstance(this);
-        EarthquakesJsonHttpResponseHandler handler = new EarthquakesJsonHttpResponseHandler();
-        Double currentLongitude = 128.2355;
-        Double currentLatitude = 3.5887;
-        Filters filter = new Filters();
-        filter.setDistance(20.0);
-        filter.setStartTime("2017-01-01");
-        client.getEarthquakeAroundPointWithFilter(128.2355, 3.5887, filter, handler);
-        ///////////////////////////////////////////////////////////////////////////////////
     }
 
 }
