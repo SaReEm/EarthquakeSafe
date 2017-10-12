07778aa353719dbc309e51e67f0d0b3f407f95ca
He Zhang <hezhang757@gmail.com>
2017-10-12 12:45:47 -0700

    Added MapScreen and got an new API key. Currently shows blank map but map loaded successfully. Need to wait for 15 min to see if the API key is working or not.

--- app/src/main/java/com/codepath/earthquakemonitor/MainActivity.java
+++ app/src/main/java/com/codepath/earthquakemonitor/MainActivity.java
@@ -21,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
+        // Handle sigin button onClick
+        Button btnLogIn = (Button) findViewById(R.id.btn_login);
+        btnLogIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(i);
+            }
+        });
+
         // Handle click sign up button
         TextView btnSignUp = (TextView) findViewById(R.id.tvSignup);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -33,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         ////Piece of code to test earthquake API /////////////////////////////////////////
-        EarthquakeClient client = EarthquakeClient.getInstance(this);
-        EarthquakesJsonHttpResponseHandler handler = new EarthquakesJsonHttpResponseHandler();
-        Double currentLongitude = 128.2355;
-        Double currentLatitude = 3.5887;
-        Filters filter = new Filters();
-        filter.setDistance(20.0);
-        filter.setStartTime("2017-01-01");
-        client.getEarthquakeAroundPointWithFilter(128.2355, 3.5887, filter, handler);
+//        EarthquakeClient client = EarthquakeClient.getInstance(this);
+//        EarthquakesJsonHttpResponseHandler handler = new EarthquakesJsonHttpResponseHandler();
+//        Double currentLongitude = 128.2355;
+//        Double currentLatitude = 3.5887;
+//        Filters filter = new Filters();
+//        filter.setDistance(20.0);
+//        filter.setStartTime("2017-01-01");
+//        client.getEarthquakeAroundPointWithFilter(128.2355, 3.5887, filter, handler);
         ///////////////////////////////////////////////////////////////////////////////////
     }
 
