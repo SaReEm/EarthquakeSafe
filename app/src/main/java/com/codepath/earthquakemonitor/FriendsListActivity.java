aa05c932d6bd642210bf4472289fe8991020f92c
He Zhang <hezhang757@gmail.com>
2017-10-13 15:17:55 -0700

    Added searchview on the action bar for friendslist.

--- app/src/main/java/com/codepath/earthquakemonitor/FriendsListActivity.java
+++ app/src/main/java/com/codepath/earthquakemonitor/FriendsListActivity.java
@@ -2,6 +2,13 @@ package com.codepath.earthquakemonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
 
 public class FriendsListActivity extends AppCompatActivity
 {
@@ -11,5 +18,40 @@ public class FriendsListActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String s)
+            {
+                // perform query here
+                //Toast.makeText(getApplicationContext(),"Perform search", Toast.LENGTH_LONG).show();
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
