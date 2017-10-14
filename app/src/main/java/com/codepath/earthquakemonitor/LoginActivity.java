40ffbc4186f0c2a70190cee02510a4573d9e64d1
Wancy <flymydream2014@gmail.com>
2017-10-14 01:06:13 -0700

    Adding user signUp and Login.

--- app/src/main/java/com/codepath/earthquakemonitor/LoginActivity.java
+++ app/src/main/java/com/codepath/earthquakemonitor/LoginActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.login.widget.LoginButton;
@@ -25,14 +27,15 @@ import io.fabric.sdk.android.Fabric;
 import static android.R.attr.permission;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etName;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-        LoginButton btnFBLogIn = findViewById(R.id.btn_fblogin);
-        //btnFBLogIn.setReadPermissions("email");
+        Button btnFBLogIn = findViewById(R.id.btn_fblogin);
         btnFBLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,15 +43,30 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        etName = findViewById(R.id.et_name);
+        etPassword = findViewById(R.id.et_password);
+
         // Handle sigin button onClick
-        Button btnLogIn = (Button) findViewById(R.id.btn_login);
-        btnLogIn.setOnClickListener(new View.OnClickListener()
-        {
+        Button btnLogIn = findViewById(R.id.btn_login);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent i = new Intent(LoginActivity.this, MapActivity.class);
-                startActivity(i);
+            public void onClick(View view) {
+                String userName = etName.getText().toString();
+                String password = etPassword.getText().toString();
+                if (userName == null || password == null) {
+                    return;
+                }
+                ParseUser.logInInBackground(userName, password, new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Intent i = new Intent(LoginActivity.this, MapActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+
             }
         });
 
@@ -84,17 +102,28 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void parseFBLogin() {
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, null, new LogInCallback() {
+        ArrayList<String> permissions = new ArrayList();
+        permissions.add("email");
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, permissions,
+                new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException err) {
-                if (user == null) {
+                if (err != null) {
+                    Log.d("MyApp", "Uh oh. Error occurred" + err.toString());
+                } else if (user == null) {
                     Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
                 } else if (user.isNew()) {
                     Log.d("MyApp", "User signed up and logged in through Facebook!");
-                    user.saveInBackground();
+                    Intent i = new Intent(LoginActivity.this, MapActivity.class);
+                    startActivity(i);
                 } else {
+                    Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT)
+                            .show();
                     Log.d("MyApp", "User logged in through Facebook!");
+                    Intent i = new Intent(LoginActivity.this, MapActivity.class);
+                    startActivity(i);
                 }
+
             }
         });
     }
