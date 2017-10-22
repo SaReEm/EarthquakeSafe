b2d19f0feff6426cbf99b9ab0920f1566c9044c0
kri55is <brisseau.emilie@gmail.com>
2017-10-22 12:00:55 -0700

    sending user safe status to server. didnt test the receiving part, code still missing on server

--- app/src/main/java/com/codepath/earthquakemonitor/ProfileActivity.java
+++ app/src/main/java/com/codepath/earthquakemonitor/ProfileActivity.java
@@ -1,14 +1,23 @@
 package com.codepath.earthquakemonitor;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.codepath.earthquakemonitor.utils.PushUtils;
 
 public class ProfileActivity extends AppCompatActivity
 {
+    private final String TAG = "ProfileActivity";
+    private Button btnLogout;
+    private Switch swMySafeStatus;
+
+    public static final String CHANNEL_NAME = "earthquake-2017";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,7 +26,7 @@ public class ProfileActivity extends AppCompatActivity
         setContentView(R.layout.activity_profile);
 
         // Handle log out onClick
-        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout = findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -27,5 +36,16 @@ public class ProfileActivity extends AppCompatActivity
                 startActivity(i);
             }
         });
+
+        swMySafeStatus = findViewById(R.id.swMySafeStatus);
+        swMySafeStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.d(TAG, "send: " + b);
+                PushUtils.sendPushNotifSafeStatus(b, CHANNEL_NAME);
+            }
+        });
     }
+
+
 }
